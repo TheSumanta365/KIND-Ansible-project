5e0eaf6444237a1d0bfe31202792b1ba6e87c5b7
TheSumanta365 <imsumanta10@gmail.com>
2025-04-29 17:19:50 +0530

    first commit

---
+++ app/Main.java
@@ -0,0 +1,29 @@
+package app;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Starting Java server on port 8080...");
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+                String response = "Hello from Java in Kubernetes!";
+                exchange.sendResponseHeaders(200, response.length());
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        });
+
+        server.start();
+    }
+}
